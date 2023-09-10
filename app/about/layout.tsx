cf5572ca958f173e3b54876fd7075261a01d9d8a
스터디하자 <hmin5015@gmail.com>
2023-09-10 15:57:11 -0700

    Add about page and error/loading/layout for about page

---
+++ app/about/layout.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+  }) {
+  return (
+    <>
+      <nav>About NavBar</nav>
+      <main className={styles.main}>
+        {children}
+      </main>
+    </>
+  )
+}
